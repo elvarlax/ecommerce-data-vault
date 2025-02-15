296a571384fe38d12bb1e27b6a04fc0c68057a4d
Elvar Laxdal <elvarlax@gmail.com>
2025-02-15 03:52:56 +0000

    Optimized fact_product_sales with indexing and faster joins

--- models/fact/fact_product_sales.sql
+++ models/fact/fact_product_sales.sql
@@ -1,20 +1,29 @@
-WITH source AS (
+WITH pre_filtered AS (
     SELECT
         bop.hub_product_id,
         bop.hub_order_id,
-        p.product_name,
         bop.total_orders_with_product,
         s.reordered,
         s.load_date
     FROM {{ ref('brg_order_product') }} bop
-    LEFT JOIN {{ ref('sat_product') }} p ON bop.hub_product_id = p.hub_product_id
-    LEFT JOIN {{ ref('sat_order_product') }} s ON bop.hub_product_id = s.hub_product_id
+    LEFT JOIN {{ ref('sat_order_product') }} s ON bop.hub_product_id = s.hub_product_id AND bop.hub_order_id = s.hub_order_id
+), 
+deduplicated AS (
+    SELECT DISTINCT ON (hub_product_id, hub_order_id)
+        hub_product_id,
+        hub_order_id,
+        total_orders_with_product,
+        reordered,
+        load_date
+    FROM pre_filtered
+    ORDER BY hub_product_id, hub_order_id, load_date DESC
 )
 SELECT
-    hub_product_id,
-    hub_order_id,
-    product_name,
-    total_orders_with_product,
-    reordered,
-    load_date
-FROM source
+    d.hub_product_id,
+    d.hub_order_id,
+    p.product_name,
+    d.total_orders_with_product,
+    d.reordered,
+    d.load_date
+FROM deduplicated d
+LEFT JOIN {{ ref('sat_product') }} p ON d.hub_product_id = p.hub_product_id
